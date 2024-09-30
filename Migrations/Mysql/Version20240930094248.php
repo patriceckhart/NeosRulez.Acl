f668049eb538a797ec26da37bdb4389b7d80bc99
Sebastian Helzle <sebastian@helzle.it>
2024-09-30 13:59:11 +0200

    TASK: Allow migration on any mysql like db

--- Migrations/Mysql/Version20240930094248.php
+++ Migrations/Mysql/Version20240930094248.php
@@ -16,10 +16,7 @@ final class Version20240930094248 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1027Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1027Platform'."
-        );
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on "mysql".');
 
         $this->addSql(
             'CREATE TABLE neosrulez_acl_domain_model_node (persistence_object_identifier VARCHAR(40) NOT NULL, nodeidentifier VARCHAR(255) NOT NULL, kind VARCHAR(255) NOT NULL, sitenodepath VARCHAR(255) NOT NULL, created DATETIME NOT NULL, PRIMARY KEY(persistence_object_identifier)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
@@ -31,10 +28,7 @@ final class Version20240930094248 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1027Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1027Platform'."
-        );
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on "mysql".');
 
         $this->addSql('DROP TABLE neosrulez_acl_domain_model_node');
         $this->addSql('DROP TABLE neosrulez_acl_domain_model_role');
